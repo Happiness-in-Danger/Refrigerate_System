37ca47bd4c3c819e15ed054e246df59c67cb1d9b
Happiness-in-Danger <45912561+Happiness-in-Danger@users.noreply.github.com>
2026-05-24 14:10:38 +0800

    Add files via upload
    Signed-off-by: Happiness-in-Danger <45912561+Happiness-in-Danger@users.noreply.github.com>

---
+++ Display_Touch/Desplay.py
@@ -0,0 +1,45 @@
+#组态屏 VTc070C334A
+import time
+from Board import board
+from Sensor.read_sensors import reads
+
+uart = board.LCD_Uart
+addre = board.REGS #(address,dtype)
+
+#write
+def uart_write(data, regs):
+    buf = bytearray()
+    
+    for x, (y, t) in zip(data, regs):
+        if t == 'u32':
+            value = int(100 * x) & 0xFFFFFFFF
+            buf += bytes([
+                0xA5, 0x5A, 0x07, 0x82,
+                (y >> 8) & 0xFF, y & 0xFF,
+                (value >> 24) & 0xFF,
+                (value >> 16) & 0xFF,
+                (value >> 8)  & 0xFF,
+                value & 0xFF,
+            ])
+        else:
+            value = int(100 * x) & 0xFFFF
+            buf += bytes([
+                0xA5, 0x5A, 0x05, 0x82,
+                (y >> 8) & 0xFF, y & 0xFF,
+                (value >> 8) & 0xFF,
+                value & 0xFF,
+            ])
+    
+    uart.write(buf)
+
+#read
+def uart_read(data, address):
+
+
+    return data
+# ------------------ run ------------------
+def desplay():
+    data_w = reads()
+    uart_write(data,addre)
+    time.sleep(0.5)
+    data_r
